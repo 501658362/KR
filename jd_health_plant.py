b8d5566053ab72f283c981832d439862fa403bb3
gaoyu <feifeigaoyu@qq.com>
2022-02-17 09:56:22 +0800

    sssssss

--- jd_health_plant.py
+++ jd_health_plant.py
@@ -566,9 +566,10 @@ def start():
                         taskName, taskId, taskToken_list = get_task2 (cookie,sid, account)
                         for i in taskToken_list:
                             do_task2 (cookie, taskName, taskId, i, sid,account)
-                    charge (charge_targe_id, cookie,sid, account)
+                    
                 except Exception as e:
                     pass
+                charge (charge_targe_id, cookie,sid, account)
         else:
             printT("请检查变量plant_cookie是否已填写")
 
