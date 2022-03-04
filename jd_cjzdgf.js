98ef8ed607f33bf722d9bc657e2ebf452f737e53
gaoyucindy <feifeigaoyu@qq.com>
2022-03-04 22:43:11 +0800

    sssssssssssss

--- jd_cjzdgf.js
+++ jd_cjzdgf.js
@@ -12,7 +12,7 @@ cron:10 10 10 10 *
 
 */
 
-let jd_cjhy_activityId = "8abe66fd0bb14f399302380d847f45bf" // 活动ID
+let jd_cjhy_activityId = "2484b97b3c0c4c9a8b4fc535d0ff68c4" // 活动ID
 let jd_cjhy_activityUrl = "https://cjhydz-isv.isvjcloud.com" // 活动地址
 
 const $ = new Env('CJ组队瓜分京豆');
