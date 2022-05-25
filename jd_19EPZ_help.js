f9e739af0d717736d1e741b658e924517d0b273f
gaoyucindy <feifeigaoyu@qq.com>
2022-05-25 20:41:58 +0800

    sssssssssssss111111111ss

--- jd_19EPZ_help.js
+++ jd_19EPZ_help.js
@@ -30,7 +30,10 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 
-$.inviteId  = [];
+$.inviteId  = ['PKASTT0184qAcF09GvBGEeUSg3QCTdWmYaRzTQjeQOc',
+'PKASTT0205KkcE2xZriW0eXmh0INuCTdWmYaRzTQjeQOc',
+    'PKASTT016aWvMlZm3Id9t9YJ1CTdWmYaRzTQjeQOc','PKASTT0146awsCVtKrAqDaQCTdWmYaRzTQjeQOc','PKASTT020vfhyRR0f_FbKKB73k_cICTdWmYaRzTQjeQOc',
+];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
